26d4d4ff0467a25db31a822f6a4c2b4bf4b291d5
scarlettperry <si.perry15@gmail.com>
2018-12-22 17:48:56 -0500

    installed papaparse, parsed csv file, reformatted urls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-console.log("hello world!")
+  let input = document.getElementById('csv')
+  input.addEventListener("change", parseData)
 
+  //parsing uploaded file object
+  function parseData(event) {
+    let file = event.target.files[0]
+    Papa.parse(file,{
+      header: true,
+      dynamticTyping: true,
+      complete: function(results){
+        reformatURL(results.data)
+      }
+    })
+  }
+  
+  //slicing URL values if begin with "[ & end with ]"
+  function reformatURL(data) {
+    console.log(data);
+    data.forEach(function(object) {
+      return console.log(object.impression_pixel_json.replace(/[\[\]""]+/g,'').split(","))
+    })
+  }
+
+  //reformating data to just have tactic id and URL
+
+  //fetch calls  if not NULL or emptry string
+
+  //array for good response - just counter
+  //array for bad response - tactic id & url
 
 
 
