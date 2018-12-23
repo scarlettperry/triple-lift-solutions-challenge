4af38e33a06299a404e511e8344fa854ffc7bd2d
scarlettperry <si.perry15@gmail.com>
2018-12-22 19:19:06 -0500

    fetch attempts made

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+  let queryString = "?tripleliftTest=true&tl_tactic_id=343664"
   let input = document.getElementById('csv')
   input.addEventListener("change", parseData)
 
@@ -10,24 +10,51 @@ document.addEventListener('DOMContentLoaded', function () {
       header: true,
       dynamticTyping: true,
       complete: function(results){
-        reformatURL(results.data)
+        fetchData(results.data)
       }
     })
   }
-  
-  //slicing URL values if begin with "[ & end with ]"
-  function reformatURL(data) {
-    console.log(data);
+
+  //fetch
+  function fetchData(data) {
+    //removing brackets and splitting string at "," if more than one url for a tactic
     data.forEach(function(object) {
-      return console.log(object.impression_pixel_json.replace(/[\[\]""]+/g,'').split(","))
+      let url = object.impression_pixel_json.replace(/[\[\]""]+/g,'').split(",")
+      if (url[0] !== "" && url[0] !== "NULL") {
+        for(let element of url){
+          fetch(element+queryString)
+            .then(function() {
+              console.log("ok")
+            }).catch(function() {
+              console.log("error")
+            })
+        }
+      }
     })
   }
 
+  //fetch calls if URL does not equal NULL or an emptry string
+  // function fetchData(data, parseData) {
+  //   console.log(parseData)
+    // fetch(data.impression_pixel_json)
+    // .then(resp => resp.json())
+    // .then(console.log);
+    // console.log(data.impression_pixel_json)
+      // console.log(data.impression_pixel_json)
+      // for(let element of data.impression_pixel_json){
+      //   console.log(element)
+      // }
+      // fetch(element.impression_pixel_json)
+      //   .then(resp => resp.json())
+      //   .then(response => console.log(response))
+
+  // }
+
   //reformating data to just have tactic id and URL
 
-  //fetch calls  if not NULL or emptry string
 
   //array for good response - just counter
+
   //array for bad response - tactic id & url
 
 
